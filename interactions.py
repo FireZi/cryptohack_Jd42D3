04ec844b91cc52267636a9fc38fa0b5ea73fd3cc
Konstantin <rybkin-k@mail.ru>
2017-12-02 20:42:09 +0300

    merge server with etherium api

--- interactions.py
+++ interactions.py
@@ -1,9 +1,11 @@
 from config import wallet_address, wallet_password
 from connector import contract_instance, web3
 
+
 def get_end_blocks():
 	result = contract_instance.getEndBlocks()
 	return result
+
 
 def check_to_end(index):
 	web3.personal.unlockAccount(wallet_address, wallet_password)
--- server.py
+++ server.py
@@ -1,27 +1,28 @@
 from time import sleep
+from interactions import get_end_blocks, check_to_end
+from connector import web3
 
 
 class Server:
 
     def __init__(self):
-        self.current_block = 0
+        self.current_block = web3.eth.blockNumber
 
     def request_blocks_data(self):
-        tmp_blocks_arr = [12, 13, 12, 11, 14, 13]
-        return tmp_blocks_arr
+        return get_end_blocks()
 
     def send_response(self, overdue_blocks):
-        pass
+        check_to_end(overdue_blocks)
 
     def get_overdue_data(self, data):
         return [i for i in range(len(data)) if data[i] <= self.current_block]
 
     def run(self):
         while True:
+            self.current_block = web3.eth.blockNumber
             blocks_data = self.request_blocks_data()
             overdue_blocks = self.get_overdue_data(blocks_data)
             self.send_response(overdue_blocks)
-            self.current_block += 1
             sleep(1)
 
 
