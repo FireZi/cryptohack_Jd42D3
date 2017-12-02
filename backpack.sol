dd4a087651724e1dbdec9ef0c0c6133e3e046fb4
EgorGornak <31937056+EgorGornak@users.noreply.github.com>
2017-12-03 01:43:14 +0300

    Add constructor

--- backpack.sol
+++ backpack.sol
@@ -2,10 +2,19 @@ pragma solidity ^0.4.5;
 
 contract backpack
 {
-    uint exchange_adress = 0x123456; // example address
-    uint Token0_address = 0x234567; 
-    uint Token1_address = 0x345678;
-    uint Token2_address = 0x456789;
+    uint exchange_adress; // example address
+    uint Token0_address;
+    uint Token1_address; 
+    uint Token2_address;
+    
+    function backpack(uint _exchange_adress, uint _Token0_address,
+                      uint _Token1_address, uint _Token2_address) {
+         exchange_adress = _exchange_adress;
+         Token0_address = _Token0_address;
+         Token1_address = _Token1_address;
+         Token2_address = _Token2_address;
+    }
+                      
     
     
     function sendToken(uint receiver, uint amount, uint value) {
