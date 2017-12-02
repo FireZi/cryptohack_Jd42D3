43ec79bb6ba76904c7b4607375cea1a76db5056f
EgorGornak <31937056+EgorGornak@users.noreply.github.com>
2017-12-03 01:56:55 +0300

    Add contract_owner & transfer()

--- backpack.sol
+++ backpack.sol
@@ -6,19 +6,21 @@ contract backpack
     uint Token0_address;
     uint Token1_address; 
     uint Token2_address;
+    uint contract_owner;
     
     function backpack(uint _exchange_adress, uint _Token0_address,
-                      uint _Token1_address, uint _Token2_address) {
+                      uint _Token1_address, uint _Token2_address, uint _contract_owner) {
          exchange_adress = _exchange_adress;
          Token0_address = _Token0_address;
          Token1_address = _Token1_address;
          Token2_address = _Token2_address;
-    }
-                      
-    
+         contract_owner = _contract_owner;
+    }                  
     
     function sendToken(uint receiver, uint amount, uint value) {
-        if (msg.sender != exchange_adress) return;
+        if (msg.sender != exchange_adress && msg.sender != contract_owner) {
+            return;
+        }
         
         if (value == 0) {
             Token0 buffer = Token0(Token0_address);
@@ -32,6 +34,11 @@ contract backpack
             Token2 buffer = Token2(Token2_address);
             buffer.send(receiver, amount);
         }
+    }
+    
+    function transfer(uint8 currencyFrom, uint8 currencyTo, uint valueFrom, uint Block) {
+        Exchange ex = Exchange(exchange_adress);
+        ex.transfer(currencyFrom, currencyTo, valueFrom, Block);
     }
 
 }
